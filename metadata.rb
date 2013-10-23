72c443846e0122b352e2a539c96148abb80b6f81
Thomas Boerger <tboerger@tbpro.de>
2013-10-23 17:31:37 +0200

    Removed zypper repository stuff

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,6 @@ license "Apache 2.0"
 description "Installs/Configures ruby"
 long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
 version "0.0.1"
-depends "zypper"
 recipe "ruby", "Installs/Configures ruby"
 
 supports "suse"
