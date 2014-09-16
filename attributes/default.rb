b9b0164e8f06500eca602d1363554b138bfe2434
Thomas Boerger <tboerger@suse.de>
2014-09-16 13:44:57 +0200

    Simplified suse repos

--- attributes/default.rb
+++ attributes/default.rb
@@ -21,6 +21,6 @@ default["ruby"]["method"] = "package"
 
 default["ruby"]["zypper"]["alias"] = "ruby-extensions"
 default["ruby"]["zypper"]["title"] = "Ruby Extensions"
-default["ruby"]["zypper"]["repo"] = "http://download.opensuse.org/repositories/devel:/languages:/ruby:/extensions/openSUSE_#{node["platform_version"] == "12.1" ? "12.3" : node["platform_version"]}/"
+default["ruby"]["zypper"]["repo"] = "http://download.opensuse.org/repositories/devel:/languages:/ruby:/extensions/openSUSE_#{node["platform_version"]}/"
 default["ruby"]["zypper"]["key"] = "#{node["ruby"]["zypper"]["repo"]}repodata/repomd.xml.key"
 
